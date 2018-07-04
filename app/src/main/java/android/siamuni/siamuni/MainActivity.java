12074d4398bf8f3b4c068c8e0d6144a33e0a5fb4
Tony <459198833@qq.com>
2018-07-04 12:20:43 +0800

    门一：创建了SiamUni项目

---
+++ app/src/main/java/android/siamuni/siamuni/MainActivity.java
@@ -0,0 +1,15 @@
+package android.siamuni.siamuni;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*门一：创建了SiamUni项目*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
